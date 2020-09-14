a16b2bbad87aa4483dda203238b9ae28ce9efbad
Alex <alex.beciana@gmail.com>
2020-09-14 15:54:46 -0400

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,9 +1,54 @@
+// import React, { Component } from 'react';
+// import Cell from './Cell'
+
+// export default class Matrix extends Component {
+
+//   // values = [
+//   //   ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+//   //   ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+//   //   ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+//   //   ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+//   //   ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+//   //   ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+//   //   ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+//   //   ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+//   //   ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+//   //   ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+//   // ]
+  
+//   genRow = (vals) => {
+//     return vals.map(val => <Cell value={val} />) // replace me and render a cell component instead!
+//   }
+  
+//   genMatrix = () => {
+//     return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
+//   }
+  
+//   render() {
+//     return (
+//       <div id="matrix">
+//         {this.genMatrix()}
+//       </div>
+//     )
+//   }
+  
+// }
+
+// Matrix.defaultProps = {
+//   values: (() => {
+//     const defRows = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+//     return (new Array(10).fill(defRows))
+//   })
+// }
+
+
 import React, { Component } from 'react';
+import Cell from './Cell.js'
 
 export default class Matrix extends Component {
   
   genRow = (vals) => {
-    return vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    return vals.map(val => <Cell value={val} />)
   }
   
   genMatrix = () => {
@@ -18,4 +63,11 @@ export default class Matrix extends Component {
     )
   }
   
+}
+
+Matrix.defaultProps = {
+  values: (() => {
+    const defRow = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+    return (new Array(10).fill(defRow))
+  })()
 }
